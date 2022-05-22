fb94b54c1d28af0bd63e6528f28c8be68a616502
Ahmed Elghandour <elghandour.12@gmail.com>
2022-05-22 15:16:10 +0200

    build banner

--- package.js
+++ package.js
@@ -4,10 +4,17 @@ module.exports = {
     buildBanner() {
         const version = pkg.version;
         const author = pkg.author;
+        const url = pkg.homepage;
+        const getYearsRange = () => {
+            return `2021 - ${new Date().getFullYear()}`;
+        };
 
-        return `
-        /* fetchify v${version}  
-        | (c) by ${author} 
+        const license = pkg.license
+
+        return `/* 
+        * fetchify v${version} (${url})
+        * Copyright ${getYearsRange()} | Author ${author}
+        * Licensed under ${license} (https://github.com/ahmedElghandour1/fetchify/blob/main/LICENSE)
         */`
 
     }
